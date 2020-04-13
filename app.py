ef09a51d692f4bc3b07f211b934d2cf806badb57
Misha Melnyk <misha@melnyk.org>
2020-04-13 17:13:23 -0400

    Run Multiple Jobs

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import math
+import sys
 
 def calcSphereVolume(radius):
     return math.pi * 4/3 * radius**3
@@ -6,5 +7,10 @@ def calcSphereVolume(radius):
 def calcCubeVolume(side):
     return side**3
 
-print(calcSphereVolume(4))
-print(calcCubeVolume(4))
+if len(sys.argv) < 2:
+    print("Not enough args")
+    exit(1)
+
+for i in sys.argv[1:]:
+    print("Sphere Volume with", i,"=",calcSphereVolume(int(i)))
+    print("Cube Volume with", i,"=",calcCubeVolume(int(i)))
